54d85640e3d95834f787c698764c671beab9433f
bartosz <bartek_krzyz@wp.pl>
2020-08-03 02:17:39 +0200

    files added to menage records creating

---
+++ app/Http/Controllers/RewardsController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reward;
+use Illuminate\Http\Request;
+
+class RewardsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('rewards.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'picture' => 'nullable',
+            'code' => 'nullable',
+            'price' => 'required',
+            'status' => 'required'
+        ]);
+        $reward = new Reward([
+            'name'    =>  $request->get('name'),
+            'picture'     =>  $request->get('picture'),
+            'code'     =>  $request->get('code'),
+            'price'     =>  $request->get('price'),
+            'status'     =>  $request->get('status')
+        ]);
+        $reward->save();
+        return redirect()->route('rewards.create')->with('success', 'Your data have been added to database successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
